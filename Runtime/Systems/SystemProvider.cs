6ebb452a1c0dda6ddac552e8f81baf9ef5338cf8
Guerro323 <kaltobattle@gmail.com>
2019-04-27 11:47:50 +0200

    added SystemProviderBatch for easier batch systems

--- Runtime/Systems/SystemProvider.cs
+++ Runtime/Systems/SystemProvider.cs
@@ -23,31 +23,59 @@ namespace StormiumTeam.GameBase
     public abstract class SystemProvider : SystemProvider<SystemProvider.NoData>
     {
         public struct NoData
-        {}
+        {
+        }
 
         public override void SpawnLocalEntityWithArguments(NoData data, ref NativeList<Entity> outputEntities)
         {
             throw new NotImplementedException();
         }
     }
-    
+
+    public abstract class SystemProviderBatch<TCreateData> : SystemProvider<TCreateData>
+        where TCreateData : struct
+    {
+        public override void SpawnBatchEntitiesWithArguments(UnsafeAllocationLength<TCreateData> array, NativeList<Entity> outputEntities, NativeList<int> indices)
+        {
+            EntityManager.CreateEntity(EntityArchetypeWithAuthority, outputEntities);
+            for (var i = 0; i != outputEntities.Length; i++)
+            {
+                var entity = outputEntities[i];
+                var data   = array[indices[i]];
+
+                SetEntityData(entity, data);
+            }
+        }
+
+        public override void SpawnLocalEntityWithArguments(TCreateData data, ref NativeList<Entity> outputEntities)
+        {
+            var entity = EntityManager.CreateEntity(EntityArchetypeWithAuthority);
+
+            SetEntityData(entity, data);
+
+            outputEntities.Add(entity);
+        }
+
+        public abstract void SetEntityData(Entity entity, TCreateData data);
+    }
+
     [UpdateInGroup(typeof(SystemProviderGroup))]
     public abstract class SystemProvider<TCreateData> : ComponentSystem, ISnapshotManageForClient, ISystemProviderExcludeComponents
-    where TCreateData : struct
+        where TCreateData : struct
     {
         private EntityModelManager m_ModelManager;
-        private GameManager m_GameManager;
-        private ModelIdent m_ModelIdent;
-        private PatternResult m_SnapshotPattern;
-        
+        private GameManager        m_GameManager;
+        private ModelIdent         m_ModelIdent;
+        private PatternResult      m_SnapshotPattern;
+
         private ComponentType[] m_EntityComponents;
         private ComponentType[] m_ExcludedComponents;
 
         private BlockComponentSerialization[] m_BlockedComponents;
 
-        public ComponentType[] EntityComponents => m_EntityComponents;
-        public ComponentType[] ExcludedComponents => m_ExcludedComponents;
-        public EntityArchetype EntityArchetype { get; protected set; }
+        public ComponentType[] EntityComponents             => m_EntityComponents;
+        public ComponentType[] ExcludedComponents           => m_ExcludedComponents;
+        public EntityArchetype EntityArchetype              { get; protected set; }
         public EntityArchetype EntityArchetypeWithAuthority { get; protected set; }
 
         public ComponentType[] ComponentsToExcludeFromStreamers { get; private set; }
@@ -55,7 +83,7 @@ namespace StormiumTeam.GameBase
         protected NativeList<TCreateData> CreateEntityDelayed;
 
         private bool m_CanHaveDelayedEntities;
-        
+
         protected override void OnCreate()
         {
             if ((m_CanHaveDelayedEntities = typeof(TCreateData) != typeof(SystemProvider.NoData)) == true)
@@ -68,7 +96,7 @@ namespace StormiumTeam.GameBase
 
         protected override void OnUpdate()
         {
-            if (m_CanHaveDelayedEntities) 
+            if (m_CanHaveDelayedEntities)
                 FlushDelayedEntities();
         }
 
@@ -76,15 +104,15 @@ namespace StormiumTeam.GameBase
         {
             if (!m_CanHaveDelayedEntities)
                 throw new NotImplementedException();
-            
+
             return CreateEntityDelayed;
         }
 
         public void FlushDelayedEntities()
         {
-            var output = new NativeList<Entity>(CreateEntityDelayed.Length, Allocator.Temp);
+            var output  = new NativeList<Entity>(CreateEntityDelayed.Length, Allocator.Temp);
             var indices = new NativeList<int>(CreateEntityDelayed.Length, Allocator.Temp);
-            
+
             SpawnBatchEntitiesWithArguments(new UnsafeAllocationLength<TCreateData>(CreateEntityDelayed), output, indices);
 
             output.Clear();
@@ -110,16 +138,16 @@ namespace StormiumTeam.GameBase
                 {
                     // todo: I was lazy when making this, this should be remade as it's slow
                     var l = new Dictionary<ComponentType, byte> {[ComponentType.ReadWrite<ModelIdent>()] = 0};
-                    
+
                     // Remove duplicates and add ModelIdent component
                     if (m_EntityComponents != null)
                         foreach (var c in m_EntityComponents)
                             l[c] = 0;
                     m_EntityComponents = l.Keys.ToArray();
                     l.Clear();
-                    
+
                     l = new Dictionary<ComponentType, byte> {[ComponentType.ReadWrite<ModelIdent>()] = 0};
-                    
+
                     if (m_ExcludedComponents != null)
                         foreach (var c in m_ExcludedComponents)
                             l[c] = 0;
@@ -138,7 +166,7 @@ namespace StormiumTeam.GameBase
                         m_BlockedComponents[i] = new BlockComponentSerialization {TypeIdx = ComponentsToExcludeFromStreamers[i].TypeIndex};
                     }
 
-                    EntityArchetype = EntityManager.CreateArchetype(EntityComponents);
+                    EntityArchetype              = EntityManager.CreateArchetype(EntityComponents);
                     EntityArchetypeWithAuthority = EntityManager.CreateArchetype(EntityComponents.Append(ComponentType.ReadWrite<EntityAuthority>()).ToArray());
 
                     var patternName = $"EntityProvider.Full.{GetType().Name}";
@@ -187,19 +215,21 @@ namespace StormiumTeam.GameBase
         {
             DestroyEntity(worldEntity);
         }
-        
+
         public virtual void GetComponents(out ComponentType[] entityComponents, out ComponentType[] excludedStreamerComponents)
         {
-            entityComponents = null;
+            entityComponents           = null;
             excludedStreamerComponents = null;
         }
-        
+
         public virtual void SerializeCollection(ref DataBufferWriter data, SnapshotReceiver receiver, SnapshotRuntime snapshotRuntime)
-        {}
-        
+        {
+        }
+
         public virtual void DeserializeCollection(ref DataBufferReader data, SnapshotSender sender, SnapshotRuntime snapshotRuntime)
-        {}
-        
+        {
+        }
+
         public virtual void SpawnBatchEntitiesWithArguments(UnsafeAllocationLength<TCreateData> array, NativeList<Entity> outputEntities, NativeList<int> indices)
         {
             var count = array.Length;
@@ -216,12 +246,12 @@ namespace StormiumTeam.GameBase
         }
 
         public abstract void SpawnLocalEntityWithArguments(TCreateData data, ref NativeList<Entity> outputEntities);
-        
+
         public virtual Entity SpawnLocalEntityDelayed(EntityCommandBuffer entityCommandBuffer)
         {
             throw new NotImplementedException();
         }
-        
+
         public Entity SpawnLocal()
         {
             return m_GameManager.SpawnLocal(GetModelIdent());
@@ -235,7 +265,7 @@ namespace StormiumTeam.GameBase
 
         public DataBufferWriter WriteData(SnapshotReceiver receiver, SnapshotRuntime runtime)
         {
-            var buffer = new DataBufferWriter(4096, Allocator.TempJob);
+            var buffer       = new DataBufferWriter(4096, Allocator.TempJob);
             var lengthMarker = buffer.WriteInt(0);
             SerializeCollection(ref buffer, receiver, runtime);
             buffer.WriteInt(buffer.Length, lengthMarker);
@@ -254,7 +284,7 @@ namespace StormiumTeam.GameBase
 
         public virtual void ExcludeComponentsFor(Type type, List<ComponentType> components)
         {
-            
+
         }
     }
 }
