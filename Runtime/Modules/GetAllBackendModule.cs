7e7a0ccd1f770ae612218f7093c70f4cbf6586b3
Guerro323 <kaltobattle@gmail.com>
2019-11-04 19:01:28 +0100

    moved 'GetAllBackendModule' from P4 package

---
+++ Runtime/Modules/GetAllBackendModule.cs
@@ -0,0 +1,87 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace StormiumTeam.GameBase.Modules
+{
+	public class GetAllBackendModule<T> : BaseSystemModule
+		where T : MonoBehaviour
+	{
+		public override ModuleUpdateType UpdateType => ModuleUpdateType.Job;
+
+		[BurstCompile]
+		private struct FindBackend : IJobForEachWithEntity<ModelParent>
+		{
+			public NativeList<Entity> MissingTargets;
+
+			public NativeList<Entity>      BackendWithoutModel;
+			public NativeList<Entity>      AttachedBackendEntities;
+			public NativeList<ModelParent> AttachedBackendDestination;
+
+			public void Execute(Entity entity, int index, ref ModelParent parent)
+			{
+				var count = MissingTargets.Length;
+				for (var i = 0; i != count; i++)
+				{
+					if (MissingTargets[i] == parent.Parent)
+					{
+						AttachedBackendEntities.Add(entity);
+						AttachedBackendDestination.Add(parent);
+
+						MissingTargets.RemoveAtSwapBack(i);
+						return;
+					}
+				}
+
+				BackendWithoutModel.Add(entity);
+			}
+		}
+
+		public NativeArray<Entity> TargetEntities;
+		public NativeList<Entity>  MissingTargets;
+
+		public NativeList<Entity>      BackendWithoutModel;
+		public NativeList<Entity>      AttachedBackendEntities;
+		public NativeList<ModelParent> AttachedBackendDestination;
+
+		private EntityQuery m_BackendQuery;
+
+		protected override void OnEnable()
+		{
+			m_BackendQuery             = System.EntityManager.CreateEntityQuery(typeof(T), typeof(ModelParent));
+			MissingTargets             = new NativeList<Entity>(Allocator.Persistent);
+			BackendWithoutModel        = new NativeList<Entity>(Allocator.Persistent);
+			AttachedBackendDestination = new NativeList<ModelParent>(Allocator.Persistent);
+			AttachedBackendEntities    = new NativeList<Entity>(Allocator.Persistent);
+		}
+
+		protected override void OnUpdate(ref JobHandle jobHandle)
+		{
+			if (!TargetEntities.IsCreated)
+				return;
+
+			BackendWithoutModel.Clear();
+			AttachedBackendEntities.Clear();
+			AttachedBackendDestination.Clear();
+
+			MissingTargets.Clear();
+			MissingTargets.AddRange(TargetEntities);
+
+			jobHandle = new FindBackend
+			{
+				MissingTargets             = MissingTargets,
+				BackendWithoutModel        = BackendWithoutModel,
+				AttachedBackendEntities    = AttachedBackendEntities,
+				AttachedBackendDestination = AttachedBackendDestination
+			}.ScheduleSingle(m_BackendQuery, jobHandle);
+		}
+
+		protected override void OnDisable()
+		{
+			AttachedBackendDestination.Dispose();
+			AttachedBackendEntities.Dispose();
+		}
+	}
+}
