9d429cca81433b34ddf57bcfaa934ed035fcc87e
Guerro323 <kaltobattle@gmail.com>
2021-08-07 14:36:03 +0200

    upgrade to b6

--- Runtime/GameHost/Core/Native.xUnity/CharBufferNativeStringExtensions.cs
+++ Runtime/GameHost/Core/Native.xUnity/CharBufferNativeStringExtensions.cs
@@ -9,21 +9,24 @@ namespace GameHost.Core.Native.xUnity
 		public static void CopyToNativeString<TCharBuffer>(this TCharBuffer buffer, ref FixedString32 nativeString)
 			where TCharBuffer : ICharBuffer
 		{
-			nativeString.CopyFrom((char*) Unsafe.AsPointer(ref buffer.Span.GetPinnableReference()), (ushort) buffer.Length);
+			nativeString.Length = 0;
+			nativeString.Append((byte*) Unsafe.AsPointer(ref buffer.Span.GetPinnableReference()), (ushort) buffer.Length);
 			nativeString.Length = buffer.Length;
 		}
 
 		public static void CopyToNativeString<TCharBuffer>(this TCharBuffer buffer, ref FixedString64 nativeString)
 			where TCharBuffer : ICharBuffer
 		{
-			nativeString.CopyFrom((char*) Unsafe.AsPointer(ref buffer.Span.GetPinnableReference()), (ushort) buffer.Length);
+			nativeString.Length = 0;
+			nativeString.Append((byte*) Unsafe.AsPointer(ref buffer.Span.GetPinnableReference()), (ushort) buffer.Length);
 			nativeString.Length = buffer.Length;
 		}
 
 		public static void CopyToNativeString<TCharBuffer>(this TCharBuffer buffer, ref FixedString128 nativeString)
 			where TCharBuffer : ICharBuffer
 		{
-			nativeString.CopyFrom((char*) Unsafe.AsPointer(ref buffer.Span.GetPinnableReference()), (ushort) buffer.Length);
+			nativeString.Length = 0;
+			nativeString.Append((byte*) Unsafe.AsPointer(ref buffer.Span.GetPinnableReference()), (ushort) buffer.Length);
 			nativeString.Length = buffer.Length;
 		}
 	}
